ddfaebbd463791049ccbbf9660db3dbc889039a1
KhanhTIMN <khanhlcbb@gmail.com>
2024-06-11 04:19:46 +0700

    Changed to Google Auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,9 +5,8 @@ import {
   type DefaultSession,
 } from "next-auth";
 
-import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { env } from "@/env.mjs";
 import { prisma } from "@/server/db";
 
 /**
@@ -48,9 +47,9 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
     }),
     /**
      * ...add more providers here.
