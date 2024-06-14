7d8c1262a125f1547da5938671dfca4e0fe11bad
Bui Hoang Anh <Anh.BH215298@sis.hust.edu.vn>
2024-06-14 17:34:23 +0700

    div div

--- src/components/page-component/example/example-02/fee.tsx
+++ src/components/page-component/example/example-02/fee.tsx
@@ -8,6 +8,7 @@ import { Separator } from "@/components/ui/separator";
 import { DataTable } from "@/components/ui/data-table/data-table";
 import {type FeeColumns} from "@/lib/validators";
 import {columns} from "@/components/page-component/example/example-02/fee-columns";
+import { ApartmentSelector } from "./apartment-selector";
 
 interface FeeClientProps {
   data: FeeColumns[];
@@ -16,24 +17,25 @@ export const FeeList = ({ data }: FeeClientProps) => {
   const router = useRouter();
 
   return (
-    <>
+    <div>
       <div className="flex items-center justify-between">
         <Heading
           title="Thông tin thu phi"
           description="Danh sách phi can thu trong hệ thống"
         />
-        <Button
+        <ApartmentSelector/>
+        {/* <Button
           onClick={() => {
             router.push("/example/payment/new");
           }}
         >
           <Plus className="mr-2 h-4 w-4" /> Thêm mới
-        </Button>
+        </Button> */}
       </div>
       <Separator />
       <div>
         <DataTable columns={columns} data={data} />
       </div>
-    </>
+    </div>
   );
 };
--- src/pages/example/example-02/index.tsx
+++ src/pages/example/example-02/index.tsx
@@ -16,18 +16,6 @@ const ApartmentSelectorPage = () => {
   return (
     <div className="flex flex-col">
       <div className="flex-1 space-y-4 p-4 md:p-8">
-        <div className="flex items-center justify-between">
-          <Heading
-            title="Quản lý khoản thu"
-            description=""
-          />
-        </div>
-        <Separator/>
-        <div className="flex items-center justify-between">
-          <h2 className="font-bold tracking-tight">Tạo khoản thu mới cho căn hộ</h2>
-        </div>
-        <ApartmentSelector/>
-        <Separator/>
         <FeeList data={data}/>
       </div>
     </div>
